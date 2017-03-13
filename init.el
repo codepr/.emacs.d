9e0d0f513ebb81289ae355f1bf12fcf63ee377cc
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2017-03-13 19:00:15 +0100

    Mar update

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 ;;; init.el --- Codep's configuration entry point.
 ;;; Commentary:
 ;; This file contain my personal Emacs configuration
@@ -7,7 +8,7 @@
 ;; Need package for package management.
 (require 'package)
 ;; Need cl because some packages load with errors otherwise
-(require 'cl)
+(eval-when-compile (require 'cl))
 ;; Get the emacs directory (same directory as this file).
 (defvar emacs-dir (file-name-directory (or load-file-name (buffer-file-name)))
   "Code directory.")
@@ -65,7 +66,7 @@
       ;; If you edit it by hand, you could mess it up, so be careful.
       ;; Your init file should contain only one such instance.
       ;; If there is more than one, they won't work right.
-      '(default ((t (:height 90 :weight Normal :family "Source Code Pro")))))
+      '(default ((t (:height 100 :weight Normal :family "Monaco")))))
      (put 'downcase-region 'disabled nil)
      (put 'upcase-region 'disabled nil)
 ))
@@ -77,10 +78,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4d80487632a0a5a72737a7fc690f1f30266668211b17ba836602a8da890c2118" default))))
+    ("4d80487632a0a5a72737a7fc690f1f30266668211b17ba836602a8da890c2118" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "9f3181dc1fabe5d58bbbda8c48ef7ece59b01bed606cfb868dd147e8b36af97c" "e4ff35d2674c6c17d889031c26578450db732349052fcf536f2e3f333f6cee29" "8556256eeea0f5b7a5d8af4a36a13c4fabeb287b2b58386255f1b2813b459dd2" "00546344a06272da82e400b4281bc142a1d194327489105fff6793f1bf9a8f3b" default)))
+ '(package-selected-packages
+   (quote
+    (base16-theme neotree which-key tao-theme spacemacs-theme solarized-theme planet-theme php-mode org-page markdown-mode magit helm-projectile helm-ag greymatters-theme grandshell-theme flycheck-haskell expand-region ensime doom-themes company-anaconda autopair auctex atom-one-dark-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 90 :weight Normal :family "Source Code Pro")))))
+ '(default ((t (:height 100 :weight Normal :family "Monaco")))))
+(setq line-spacing 3)
